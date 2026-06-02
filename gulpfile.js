783e87625c785ed06dc474b8bc13cf2bc0681428
fewfre <wesitup@gmail.com>
2026-06-02 19:08:22 -0400

    small gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var tsify = require('tsify');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const tsify = require('tsify');
 
 function core() {
 	return browserify({
@@ -12,25 +12,22 @@ function core() {
 		cache: {},
 		packageCache: {}
 	})
-	.plugin(tsify)
-	.bundle()
-	.pipe(source('core.js'))
-	.pipe(buffer()) // <----- convert from streaming to buffered vinyl file object - needed for "uglify" to work
-	.pipe(uglify())
-	.pipe(gulp.dest('build'));
+		.plugin(tsify)
+		.bundle()
+		.pipe(source('core.js'))
+		.pipe(buffer()) // converts from streaming to buffered vinyl file object - needed for "uglify" to work
+		.pipe(uglify())
+		.pipe(gulp.dest('build'));
 }
 
 function copyImages() {
-	return gulp.src(['images/*'], {
-		base: '',
-		encoding: false
-	}).pipe(gulp.dest('build/images'));
+	return gulp.src('images/*', { base: '', encoding: false })
+		.pipe(gulp.dest('build/images'));
 }
 
 function copyWeb() {
-	return gulp.src(['web/*'], {
-		base: ''
-	}).pipe(gulp.dest('build'));
+	return gulp.src('web/*', { base: '' })
+		.pipe(gulp.dest('build'));
 }
 
 exports.core = core;
